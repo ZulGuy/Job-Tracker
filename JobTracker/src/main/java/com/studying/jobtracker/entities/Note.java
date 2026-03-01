7065633b5138cc556561044549ba94a55ef70ea1
ZulGuy <frostborne1@gmail.com>
2026-03-02 00:58:35 +0200

    Created Job Tracker project

--- JobTracker/src/main/java/com/studying/jobtracker/entities/Note.java
+++ JobTracker/src/main/java/com/studying/jobtracker/entities/Note.java
@@ -5,4 +5,37 @@ public class Note {
   private int id;
   private String content;
   private User user;
+
+  public Note(int id, String content, User user) {
+    this.id = id;
+    this.content = content;
+    this.user = user;
+  }
+
+  public Note() {
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
 }
--- JobTracker/src/main/java/com/studying/jobtracker/entities/User.java
+++ JobTracker/src/main/java/com/studying/jobtracker/entities/User.java
@@ -1,4 +1,5 @@
 package com.studying.jobtracker.entities;
 
 public class User {
+
 }
