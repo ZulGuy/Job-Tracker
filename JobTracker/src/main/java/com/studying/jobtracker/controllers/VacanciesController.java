c515066e82596eda6504e188a32011c5e1a70bc8
ZulGuy <frostborne1@gmail.com>
2026-03-14 11:59:54 +0200

    Job-Tracker. Implementing controllers.

--- JobTracker/src/main/java/com/studying/jobtracker/controllers/VacanciesController.java
+++ JobTracker/src/main/java/com/studying/jobtracker/controllers/VacanciesController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,10 +21,22 @@ public class VacanciesController {
   public VacanciesController(VacanciesService vacanciesService) {
     this.vacanciesService = vacanciesService;
   }
-  
+
   @GetMapping
   public ResponseEntity<List<Vacancy>> getAllVacancies() {
     return new ResponseEntity<>(vacanciesService.getAllVacancies(), HttpStatus.OK);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Vacancy> getVacancyById(
+      @PathVariable int id) {
+    return new ResponseEntity<>(vacanciesService.findById(id), HttpStatus.OK);
+  }
+
+  @GetMapping("/{name}")
+  public ResponseEntity<Vacancy> getVacancyByName(
+      @PathVariable String name) {
+    return new ResponseEntity<>(vacanciesService.findByName(name), HttpStatus.OK);
   }
 
 }
